cbf2c6cbf849e49e14bf809e2677a9f7911666b7
Dave <dave@dhills.net>
2024-05-28 15:42:54 +0100

    Fixed file tree key bind

--- config/default.nix
+++ config/default.nix
@@ -3,26 +3,32 @@
   imports = [ ./bufferline.nix ./telescope.nix ./which-key.nix ./treesitter.nix ./lsp.nix ];
   colorschemes.ayu.enable = true;
   globals.mapleader = ",";
-  plugins.toggleterm.enable = true;
-  # Possible plugins to try:
-  #surround = extra motions for surrounding text
-  #spider = moving by words works differently (e.g. sub-words)
-  #statuscol = status column - I started some setup, but couldn't seem to get it working got a sub-module ready for it
-  # neoscroll
-  # noice
-  # smart-splits or tmux integration
-
-  plugins.nvim-tree.enable = true;
-  plugins.nvim-tree.updateFocusedFile.enable = true;
-  plugins.nvim-tree.updateFocusedFile.updateRoot = true;
-  plugins.notify.enable = true;
-  plugins.nix.enable = true;
-  plugins.neoscroll.enable = true;
-  keymaps = [
+  plugins = {
+    tmux-navigator.enable = true;
+    toggleterm.enable = true;
+    # Possible plugins to try:
+    #surround = extra motions for surrounding text
+    #spider = moving by words works differently (e.g. sub-words)
+    #statuscol = status column - I started some setup, but couldn't seem to get it working got a sub-module ready for it
+    # neoscroll
+    # noice
+    nvim-tree = {
+      enable = true;
+      updateFocusedFile.enable = true;
+      updateFocusedFile.updateRoot = true;
+    };
+    notify.enable = true;
+    nix.enable = true;
+    neoscroll.enable = true;
+};
+ keymaps = [
     {
-      action = "NvimTreeOpen";
-      key = "<Leader>t";
-      options.desc = "Open current folder in Nvim-tree";
+      action = "<cmd>NvimTreeToggle<CR>";
+      key = "<leader>t";
+      options = {
+        silent = true;
+        desc = "Toggle File Tree";
+      };
     }
   ];
 }
