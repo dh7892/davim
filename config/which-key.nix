06d9eca4a0a7fadf4c46a1a0de2fef7f0cb5e489
Dave <dave@dhills.net>
2024-11-20 12:13:30 +0000

    Removed un-needed descriptions

--- config/which-key.nix
+++ config/which-key.nix
@@ -1,13 +1,5 @@
 {
   plugins.which-key = {
     enable = true;
-    settings.spec = [
-      {
-        __unkeyed-1 = "<leader>e";
-        desc = "Bimble"; 
-      }
-    # registrations = { "<leader>g" = "Grep string with telescope"; };
-    # registrations = { "<leader>l" = "Live grep with telescope"; };
-    ];
   };
 }
