5f51d607efd4dc297d8fb9dd9dee97d8fb8da14b
Dave <dave@dhills.net>
2025-01-27 11:37:04 +0000

    updated sha

--- config/venv-select.nix
+++ config/venv-select.nix
@@ -7,6 +7,6 @@ pkgs.vimUtils.buildVimPlugin {
     owner = "linux-cultist";
     repo = "venv-selector.nvim";
     rev = "9b1bfe5aea225853e14358f504c656d4af007035";
-    sha256 = "sha256-PTL0wwUE1sO6YlJNPnlNilKyR5kQDBYXiDM5gh6pkuM=";
+    sha256 = "sha256-XY4hHvslNginl+FxT7owjobInmyhRDwPtNaWOAPpOO0=";
   };
 }
